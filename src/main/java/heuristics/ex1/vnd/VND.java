c9690132fbd7c6408d6174b1b76a36963c06c6a2
Stefan Heigl <stefan.heigl90@gmail.com>
2018-11-24 08:23:49 +0100

    add time out to vnd

--- src/main/java/heuristics/ex1/vnd/VND.java
+++ src/main/java/heuristics/ex1/vnd/VND.java
@@ -4,22 +4,36 @@ import heuristics.ex1.dto.Graph;
 import heuristics.ex1.dto.Solution;
 import heuristics.ex1.localsearch.neighborhood.*;
 
+import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.List;
 
 public class VND {
 
+    private int maxTimeInSeconds;
     private List<Neighborhood> neighborhoods = new ArrayList<>();
 
     public VND() {
+        this(15 * 60);
+    }
+
+    public VND(int maxTimeInSeconds) {
+        this.maxTimeInSeconds = maxTimeInSeconds;
+
         neighborhoods.add(new TwoOptNeighborhood());
         neighborhoods.add(new TwoFiveOptNeighborhood());
         neighborhoods.add(new ThreeOptNeighborhoodNew());
     }
 
     public Solution improve(Solution solution, Graph graph) {
-        int l = 0;
 
+        long startTime = ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime();
+        long endTime = startTime + 1000L * 1000L * 1000L * maxTimeInSeconds;
+
+        boolean timedOut = false;
+
+
+        int l = 0;
         do {
             Neighborhood neighborhood = neighborhoods.get(l);
 
@@ -31,7 +45,13 @@ public class VND {
             } else {
                 l++;
             }
-        } while (l < neighborhoods.size());
+
+            long currentTime = ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime();
+            if (currentTime >= endTime) {
+                timedOut = true;
+                solution.setTimedOut(true);
+            }
+        } while (l < neighborhoods.size() && !timedOut);
 
         return solution;
     }
