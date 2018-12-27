f03cdb7fddd7cb57628dbd8a69bf61dcd1e34989
Stefan Heigl <stefan.heigl90@gmail.com>
2018-12-27 07:57:58 +0100

    improve randomized greedy search

--- src/main/java/heuristics/ex1/construction/RandomizedGreedySearch.java
+++ src/main/java/heuristics/ex1/construction/RandomizedGreedySearch.java
@@ -2,7 +2,6 @@ package heuristics.ex1.construction;
 
 import heuristics.ex1.dto.Graph;
 import heuristics.ex1.dto.Solution;
-import heuristics.ex2.ga.util.Recombinator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -12,7 +11,7 @@ import java.util.stream.Collectors;
 public class RandomizedGreedySearch implements ConstructionHeuristic {
 
     private static final int RESTRICTED_CANDIDATE_LIST_FACTOR_DEFAULT = 10;
-    private int restrictedCandidateListFactor = 10;
+    private int restrictedCandidateListFactor;
 
     public RandomizedGreedySearch(int restrictedCandidateListFactor) {
         this.restrictedCandidateListFactor = restrictedCandidateListFactor;
@@ -31,17 +30,16 @@ public class RandomizedGreedySearch implements ConstructionHeuristic {
         Set<Integer> nodes = graph.getMatrix().keySet();
 
         int firstNode = nodes.stream()
-                             .findFirst()
-                             .orElseThrow(() -> new RuntimeException("Graph has no nodes"));
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Graph has no nodes"));
         solution.add(firstNode);
 
+        List<Integer> candidateList = new LinkedList<>(nodes);
+        candidateList.remove((Integer) firstNode);
+
+        int lastNode = firstNode;
+
         while (solution.size() < nodes.size()) {
-            int lastNode = solution.get(solution.size()-1);
-
-            List<Integer> candidateList = nodes.stream()
-                                                .filter(n -> !solution.contains(n))
-                                                .collect(Collectors.toList());
-
             List<Integer> restrictedCandidateList = restrict(candidateList, objective, lastNode, graph);
 
             Random random = new Random();
@@ -52,9 +50,11 @@ public class RandomizedGreedySearch implements ConstructionHeuristic {
 
             solution.add(node);
             objective += weight;
+
+            candidateList.remove((Integer) node);
+            lastNode = node;
         }
 
-        int lastNode = solution.getLast();
         objective += graph.getWeight(lastNode, firstNode);
 
         return new Solution(solution, objective);
@@ -63,17 +63,17 @@ public class RandomizedGreedySearch implements ConstructionHeuristic {
     private List<Integer> restrict(List<Integer> candidateList, long objective, int lastNode, Graph graph) {
 
         List<NodeWithObjective> nodeWithObjectives = candidateList.stream()
-                                        .map(c -> new NodeWithObjective(c, Math.abs(objective + graph.getWeight(lastNode, c))))
-                                        .collect(Collectors.toList());
+                .map(c -> new NodeWithObjective(c, Math.abs(objective + graph.getWeight(lastNode, c))))
+                .collect(Collectors.toList());
 
         List<NodeWithObjective> sorted = nodeWithObjectives.stream()
-                                        .sorted(Comparator.comparingLong(NodeWithObjective::getObjective))
-                                        .collect(Collectors.toList());
+                .sorted(Comparator.comparingLong(NodeWithObjective::getObjective))
+                .collect(Collectors.toList());
 
         return sorted.stream()
-                     .limit(graph.getMatrix().size() / restrictedCandidateListFactor)
-                     .map(NodeWithObjective::getNode)
-                     .collect(Collectors.toList());
+                .limit(graph.getMatrix().size() / restrictedCandidateListFactor)
+                .map(NodeWithObjective::getNode)
+                .collect(Collectors.toList());
     }
 
     @AllArgsConstructor
